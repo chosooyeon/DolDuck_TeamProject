1fb9bb5d855596f178233b2e312e483785d68412
HanJS <lotus526@naver.com>
2019-08-06 13:33:41 +0900

    chat update v1.4
    - Server is upgraded to HTTPS
    - make a chat room

--- dolduck_project/src/main/webapp/resources/node-server/index.js
+++ dolduck_project/src/main/webapp/resources/node-server/index.js
@@ -1,25 +1,72 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var https = require('https');
-var io = require('socket.io').listen(http);
-var fs = require('fs');
+const express = require('express')
+const https = require('https')
+const socketIO = require('socket.io')
+const fs = require('fs')
+
+var app = express()
+
+const options = {
+    key: fs.readFileSync('./key/server.key'),
+    cert: fs.readFileSync('./key/server.cert')
+}
+
+app.use('/css', express.static('./css'))
+app.use('/js', express.static('./js'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/chatroom.html');
 });
 
+const server = https.createServer(options, app)
+
+const io = socketIO(server)
+
 io.sockets.on('connection', (socket) => {
-    var socketId = socket.id;
-    var clientIp = socket.request.connection.remoteAddress;    //접속한 클라이트의 ip 추적  
-    console.log('New Connection from '+ clientIp);
-    
+
+    var room
+
+    socket.on('joinRoom', function (name, _room) {
+
+        room = _room
+
+        socket.name = name
+        socket.join(room)
+        console.log(`user [${socket.name}] joined ${room}`);
+
+        io.sockets.to(room).emit('joinedRoom', {
+            room: room,
+            name: socket.name
+        })
+    })
+
+   /*  socket.on('joinRoom', (name, num) => {
+        socket.join(room, () => {
+            console.log(`${name} join a  room (${room})`);
+            io.sockets.to(room).emit('joinRoom', {
+                room : num,
+                name : name
+            })            
+        })
+    })
+    */
+
+    socket.on('leaveRoom', (name, num) => {
+        socket.leave(num, () => {
+            console.log(`${name} leave a room (${num})`);
+            io.to(num).emit('leaveRoom', name, num)
+        }) 
+     })
+ 
     socket.on('chat message', (msg) => {
         //소켓을 통해 이벤트 전송
-        io.emit('data', {"chatMessage" : msg, "clientIp" : clientIp });
-        console.log('['+clientIp+'] sent >>' + msg);
+        socket.broadcast.to(room).emit('message', { type: 'msg', chatMessage: msg })
+    });
+
+    socket.on('disconnect', (name) => {
+          console.log(`user [${socket.name}] disconnected`);
     });
 });
 
-http.listen(3000, function(){
+server.listen(3000, function () {
     console.log('listening on port : 3000');
 });
