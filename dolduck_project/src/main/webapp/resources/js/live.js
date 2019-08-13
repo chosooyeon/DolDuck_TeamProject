943ee5d557728c3b31272b31332aa36cbc52220b
HanJS <lotus526@naver.com>
2019-08-13 14:03:01 +0900

    LiveBoard v1.1
    LiveBoard v1.1

--- dolduck_project/src/main/webapp/resources/js/live.js
+++ dolduck_project/src/main/webapp/resources/js/live.js
@@ -1,2 +1,42 @@
 
+var contentArea = document.getElementById('live-content')
 
+//csrf Token
+var token = $("meta[name='_csrf']").attr("content");
+var header = $("meta[name='_csrf_header']").attr("content");
+
+$(document).ready(function() {
+	
+	$('.nav > li > a').on('click', function() {
+		var tab = $('.nav > li > a').val()
+
+		switch(tab){
+			case '홈': 
+				handleLiveBoard()
+				break;
+			case '일정표':
+				break;
+			case '온에어':
+				break;
+			case '채널보기':
+				break;
+			default : break;
+		}
+		
+	})
+})
+
+function handleLiveBoard(){
+	$.ajax({
+		type : 'POST', 
+		url : '',
+		dataType : 'json',
+		beforeSend : function(xhr){
+			xhr.setRequestHeader(header, token)
+		}, success : function(data){
+			console.log(`Success to get ${data}`)
+		}, error: function(e){
+			console.log(`Error: ${e}`)
+		}
+	})
+} 
