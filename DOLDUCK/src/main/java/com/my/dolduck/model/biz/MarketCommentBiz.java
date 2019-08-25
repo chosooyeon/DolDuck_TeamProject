255a622ac92869679e4db8ccc7bd4f3ce1f8bde1
chosooyeon <1whtndus@naver.com>
2019-08-25 15:35:01 +0900

    market 추가

--- DOLDUCK/src/main/java/com/my/dolduck/model/biz/MarketCommentBiz.java
+++ DOLDUCK/src/main/java/com/my/dolduck/model/biz/MarketCommentBiz.java
@@ -1,13 +1,11 @@
-package com.my.dolduck.model.dao;
+package com.my.dolduck.model.biz;
 
 import java.util.List;
 
 import com.my.dolduck.model.dto.MarketCommentDto;
 
-public interface MarketCommentDao {
-	
-	String namespace="market_comment.";
-	
+public interface MarketCommentBiz {
+
 	public List<MarketCommentDto> market_comment_list(int market_num);
 	public MarketCommentDto market_comment_detail(String id);
 	public int market_comment_insert(MarketCommentDto dto);
