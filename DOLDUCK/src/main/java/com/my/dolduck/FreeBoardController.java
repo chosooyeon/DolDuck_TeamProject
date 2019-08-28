c92c02650506a2060233c9c7cab6d54281a0935f
github leehan1431 <leehan1431@naver.com>
2019-08-28 11:02:47 +0900

    파일업로드

--- DOLDUCK/src/main/java/com/my/dolduck/FreeBoardController.java
+++ DOLDUCK/src/main/java/com/my/dolduck/FreeBoardController.java
@@ -89,7 +89,11 @@ public class FreeBoardController {
 			System.out.println("fileSize : " + fileSize);
 			int res = 0;
 			try {
-				mf.transferTo(new File(class_img_path)); // 파일 집어넣는다
+				if(mf.getSize() == 0) {
+					System.out.println("없음");
+				}else {
+					mf.transferTo(new File(class_img_path)); // 파일 집어넣는다					
+				}
 
 				res = biz.free_insert(dto);
 				if (res > 0) {
@@ -179,8 +183,12 @@ public class FreeBoardController {
 
 			int res = 0;
 			try {
-				mf.transferTo(new File(class_img_path)); // 파일 집어넣는다
-
+				
+				if(mf.getSize() == 0) {
+					System.out.println("없음");
+				}else {
+					mf.transferTo(new File(class_img_path)); // 파일 집어넣는다					
+				}
 				res = biz.free_update(dto);
 				if (res > 0) {
 					System.out.println("성공");
@@ -195,8 +203,6 @@ public class FreeBoardController {
 				e.printStackTrace();
 			}
 		}
-
-		System.out.println("실패");
 
 		return "redirect:free_update.do";
 
