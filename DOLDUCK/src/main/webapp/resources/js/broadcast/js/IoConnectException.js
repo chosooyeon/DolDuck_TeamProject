3e3532ad39c74e6ba7405fb4c935c6603c500f25
github leehan1431 <leehan1431@naver.com>
2019-08-30 13:28:35 +0900

    수정본

--- DOLDUCK/src/main/webapp/resources/js/broadcast/js/IoConnectException.js
+++
@@ -1,8 +0,0 @@
-try {
-    var socket = io.connect('https://192.168.10.107:5571', { rejectUnauthorized: false })
-	//var socket = io.connect('https://192.168.10.107:5571', { rejectUnauthorized: false });
-} catch (error) {
-    console.log('Socket.io Exception : ', error)
-    location.href = 'https://192.168.10.107:5571/IoException'
-    //location.href = 'https://192.168.10.169:5571/IoException'
-}
--- DOLDUCK/src/main/webapp/resources/js/live/live-start.js
+++
@@ -1,6 +0,0 @@
-
-var startLiveBtn = document.getElementById('btn-startlive')
-startLiveBtn.addEventListener('click', ()=>{ 
-    random = parseInt(Math.random()*999999999999)
-    location.href = `/caster/${random}` 
-})
